36140a4697222719de5d72e4a6ae944173e58340
Berke Öncül <berkeoncul4@gmail.com>
2020-12-28 23:10:53 +0300

    Update database.js

--- haberdobra api/config/database.js
+++ haberdobra api/config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports={
   
     username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || "golat2052",
+    password: process.env.DB_PASSWORD || "",
     database: process.env.DB_DATABASE ||"haberdobra",
     host:     process.env.DB_HOST ||"127.0.0.1",
     dialect:  process.env.DB_DIALECT ||"mysql",
